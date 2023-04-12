3473261aa910c2f854eb2cf243cb39c79710fe86
pablo vasquez <pablovasquez123321@icloud.com>
2023-04-12 19:59:30 -0300

    preentregaaa

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function registrarUsuario() {
-    alert("Bienvenido a _Start la tienda gamer N1 del país! \n Ingrese sus datos.");
+    alert("Bienvenido a _Start! \n Ingrese sus datos.");
 
     let usuario = {};
 
@@ -17,4 +17,95 @@ function registrarUsuario() {
 }
 
 registrarUsuario();
+let presupuestoUsuario = Number(prompt("Ingresa el monto de tu presupuesto"));
+let total = 0;
+let totalCompra = 0;
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+
+const listaDeCompras = [
+    new Producto("Xbox series S", 160000),
+    new Producto("Xbox series X", 200000),
+    new Producto("PlayStation 4", 150000),
+    new Producto("PlayStation 5", 300000),
+    new Producto("Nintendo switch", 280000),
+];
+
+
+let carrito = [];
+let seleccion = prompt(`¿Desea agregar un producto? "si" o "no"`);
+
+while (seleccion !== "no" && seleccion !== "si") {
+    alert("Por favor ingrese una respuesta válida");
+    seleccion = prompt(`¿Desea agregar un producto? "si" o "no"`);
+}
+
+while (seleccion !== "no") {
+    alert("Elija el producto que deseas comprar");
+    let todosLosProductos = listaDeCompras.map((producto) => `${producto.nombre} $${producto.precio}`).join("\n");
+    alert(todosLosProductos);
+
+    let producto = prompt("Agrega un producto a tu carrito:");
+    let precio = 0;
+    
+    switch (producto) {
+        case "xbox series s":
+            precio = 160000;
+            break;
+        case "xbox series x":
+            precio = 200000;
+            break;
+        case "playstation 4":
+            precio = 150000;
+            break;
+        case "playstation 5":
+            precio = 300000;
+            break;
+        case "nintendo switch":
+            precio = 280000;
+            break;
+        default:
+            alert("No tenemos stock de ese producto.");
+            continue; 
+    }
+
+    let unidades = parseInt(prompt("Indique cuántas unidades quiere agregar:"));
+
+
+    if (isNaN(unidades) || unidades <= 0) {
+        alert("Ingrese una cantidad de unidades válida.");
+        continue; 
+    }
+
+    carrito.push({ producto, precio });
+    console.log(carrito);
+
+    seleccion = prompt("¿Quiere seguir comprando? (sí o no)");
+}
+
+
+if (carrito.length === 0) {
+    alert("No se agregó ningún producto.");
+} else {
+
+    alert("Detalle de la compra:");
+    carrito.forEach((producto) => {
+        alert(`Producto: ${producto.producto}, Unidades: ${producto.unidades}, Total a pagar por producto: $${producto.unidades * producto.precio}`);
+    });
+
+
+    const totalCompra = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+    if (totalCompra > presupuestoUsuario) {
+        alert(`Lo siento, no tienes suficiente presupuesto para comprar todos los productos. Tu presupuesto es de $${presupuestoUsuario} y tu total a pagar es de $${totalCompra}.`);
+    } else {
+        alert(`Tu compra ha sido realizada con éxito. Tu presupuesto es de $${presupuestoUsuario} y tu total a pagar es de $${totalCompra}.`);
+    }
+}
+
 
